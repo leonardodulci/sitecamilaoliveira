3c45bf29fda69030073a6a5ec00734d5f1d54f5b
Leonardo Silva Dulci <leonardodulci@gmail.com>
2025-08-29 15:35:39 -0300

    updates

---
+++ header(rolagem).js
@@ -0,0 +1,8 @@
+const header = document.querySelector('.main-header');
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 50) { // Quando o usuário rolar 50px para baixo
+            header.classList.add('scrolled');
+        } else {
+            header.classList.remove('scrolled');
+        }
+    });
