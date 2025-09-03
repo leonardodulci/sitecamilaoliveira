4d4b92f526ce3ad96033b149c24977579956c9db
Leonardo Silva Dulci <leonardodulci@gmail.com>
2025-09-03 09:44:03 -0300

    update

--- public/formWpp.js
+++ public/formWpp.js
@@ -59,3 +59,25 @@ document.addEventListener('DOMContentLoaded', () => {
     // Define o intervalo para a troca de avaliações a cada 1 minuto (60000ms)
     setInterval(showNextGroup,10000);
 });
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCoxGcEOlyWZxuMpP4pv1JzyHoWOfsJrO8",
+  authDomain: "site-camilaoliveira.firebaseapp.com",
+  projectId: "site-camilaoliveira",
+  storageBucket: "site-camilaoliveira.firebasestorage.app",
+  messagingSenderId: "1050196958342",
+  appId: "1:1050196958342:web:8f00851b94d791a6cd0507",
+  measurementId: "G-5N1FPQFXQV"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
